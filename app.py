d9fbee7caa01ba086dc4cb82c8bc5c9eb83c299b
Shao0203 <nzsyl0203@gmail.com>
2025-10-11 21:07:27 +0800

    Python-100-Days-17 functions: lambda, decorator, recursion

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import random
+import string
+import math
+import operator
+import time
+import functools
 
 
 # # # 1. 素数
@@ -48,7 +53,8 @@ import random
 #         print(i)
 
 
-# # 2. 最大公约数
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
+# # 2. 最大公约数 / 最小公倍数
 # x = int(input('x = '))
 # y = int(input('y = '))
 # for i in range(x, 0, -1):
@@ -65,7 +71,21 @@ import random
 #     a, b = b, a % b     # a 变成原来的 b, b 变成余数 a%b
 # print(f'{b} 是最大公约数')
 
+# def gcd(a, b):
+#     while a % b != 0:
+#         a, b = b, a % b
+#     return b
 
+
+# def lcm(a, b):
+#     return a * b // gcd(a, b)
+
+
+# print(f'{gcd(36, 48)}是最大公约数')
+# print(f'{lcm(36, 48)}是最小公倍数')
+
+
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
 # # 3. 猜数字
 # answer = random.randrange(1, 101)
 # counter = 0
@@ -81,6 +101,7 @@ import random
 #         break
 
 
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
 # # 4. 斐波那契数列 Fibonacci sequence
 # # 4.1
 # def fibonacci(num):
@@ -119,6 +140,7 @@ import random
 #     a, b = b, a + b
 
 
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
 # # 5. 寻找水仙花数narcissistic number,: 153 = 1**3+5**3+3**3, 1634 = 1**4+6**4+3**4+4**4
 # 拆分一个数的小技巧 百位: num // 100  十位: num % 100 // 10   个位数: num % 10
 # for num in range(100, 1000):
@@ -129,6 +151,7 @@ import random
 #         print(num)
 
 
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
 # # 6. 数字反转: 12389 -> 98321
 # # num		reversed_num
 # # 12389	0
@@ -145,6 +168,7 @@ import random
 # print(reversed_num)
 
 
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
 # # 7. 百钱百鸡问题
 # for x in range(0, 21):
 #     for y in range(0, 34):
@@ -159,6 +183,7 @@ import random
 #             print(f'公鸡: {x}只, 母鸡: {y}只, 小鸡: {z}只')
 
 
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
 # # 8. 6面色子掷6000次，记录每面多少次
 # import random
 
@@ -171,6 +196,7 @@ import random
 # print(f'一共抛了{sum(counters)}次色子。')
 
 
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
 # # 9. 构造一个字典，用zip方式
 # item1 = dict(zip('abcdefg', list(range(1, 8))))
 # print(item1)
@@ -180,6 +206,7 @@ import random
 # print(item2)
 
 
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
 # # 10. 查找一句话中每个字母出现字数，从高到低排序
 # message = 'Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.'
 # counter = {}
@@ -195,6 +222,7 @@ import random
 # print(dict(result))
 
 
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
 # # 11. 股票排序筛选高价
 # stocks = {
 #     'AAPL': 191.88,
@@ -214,3 +242,97 @@ import random
 # print(stock3)
 # sorted_stocks = dict(sorted(stocks.items(), key=lambda x: x[1], reverse=True))
 # print(sorted_stocks)
+
+
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
+# # 12. 装饰器函数Decorator
+# def my_decorator(func):
+#     def wrapper(*args, **kwargs):
+#         print('Start')
+#         result = func(*args, **kwargs)
+#         print('End...')
+#         return result
+#     return wrapper
+
+
+# @my_decorator
+# def greeting(name, age):
+#     return f'Hi {name}, you are {age} years old.'
+
+
+# print(greeting('bright', 10))   # Start     End     Hi bright, you are 10 years old.
+
+
+# def record_time(func):
+#     # 想去掉装饰器的作用执行原函数, wraps函数放在wrapper函数上, 保留被装饰之前的函数,__wrapped__属性获得被装饰之前的函数
+#     @functools.wraps(func)
+#     def wrapper(*args, **kwargs):
+#         start = time.time()
+#         result = func(*args, **kwargs)
+#         end = time.time()
+#         print(f'{func.__name__}执行时间: {end - start:.2f}秒')
+#         return result
+#     return wrapper
+
+
+# @record_time
+# def download(filename):
+#     print(f'开始下载{filename}.')
+#     time.sleep(random.random() * 6)
+#     print(f'{filename}下载完成.')
+
+
+# @record_time
+# def upload(filename):
+#     print(f'开始上传{filename}.')
+#     time.sleep(random.random() * 6)
+#     print(f'{filename}上传完成.')
+
+# # 调用装饰后的函数会记录执行时间
+# download('test.pdf')
+# upload('yy.avi')
+# # 取消装饰器的作用不记录执行时间
+# download.__wrapped__('MySQL必知必会.pdf')
+# upload.__wrapped__('Python从新手到大师.pdf')
+
+
+##### -----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~-----~~~~~#####
+# # 13. 递归Recursion， 阶乘math.factorial(5)
+# def fac(num):
+#     if num in (0, 1):   # 收敛条件
+#         return 1
+#     return num * fac(num - 1)   # 递归公式
+
+
+# print(fac(5))
+
+# 递归调用函数入栈
+# 5 * fac(4)
+# 5 * (4 * fac(3))
+# 5 * (4 * (3 * fac(2)))
+# 5 * (4 * (3 * (2 * fac(1))))
+# 停止递归函数出栈
+# 5 * (4 * (3 * (2 * 1)))
+# 5 * (4 * (3 * 2))
+# 5 * (4 * 6)
+# 5 * 24
+# 120
+
+# def fac(num):
+#     result = 1
+#     for i in range(2, num + 1):
+#         result *= i
+#     return result
+
+# print(fac(5))
+
+# @functools.lru_cache()
+# def fib(n):
+#     # 执行性能糟糕, 所以用上面的lru_cache缓存执行结果从而避免在递归调用的过程中产生大量的重复运算
+#     if n in (1, 2):
+#         return 1
+#     return fib(n-1) + fib(n-2)
+
+
+# for i in range(1, 51):
+#     print(fib(i))
